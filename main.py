7b299f560cf3e3cbbd878cf7e40aca10c8e35408
RadRoamer <zasypkovlad209@gmail.com>
2024-09-16 23:41:29 +0300

    add logging

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ from dotenv import dotenv_values
 config = dotenv_values()
 api_key = config['API_KEY']
 
+
+logging.basicConfig(
+    format="[%(asctime)s] | %(filename)-20s:%(lineno)3d | %(levelname)-7s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+    )
+logging.warning('log into app account')
+
 # instantiate PyUnsplash object
 py_un = PyUnsplash(api_key=api_key)
 
-
-#
 def main():
+    logging.warning('search required photos')
     search = py_un.search(type_='photos', query='redhead beauty', per_page=5)
     for photo in search.entries:
-        print(photo.id, photo.link_download)
+        print(photo.link_download)
 
 
 if __name__ == '__main__':

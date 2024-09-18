a64b23af8a71d5eb796b5f53371e3b69e52bdd76
RadRoamer <zasypkovlad209@gmail.com>
2024-09-18 10:54:35 +0300

    downloading file fucntion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+import requests
+from requests.exceptions import HTTPError
 from pyunsplash import PyUnsplash
 from dotenv import dotenv_values
 
@@ -15,11 +17,30 @@ logging.warning('log into app account')
 # instantiate PyUnsplash object
 py_un = PyUnsplash(api_key=api_key)
 
+def download_images(link: str):
+    """
+    downloading image from https://unsplash.com via requests library
+    :param link: link to the photo. (link example: photo_object.body['urls']['full'])
+
+    """
+    try:
+        response = requests.get(link, allow_redirects=True)
+        # Raises an exception if status code is not 2xx
+        response.raise_for_status()
+
+        logging.warning('successfully connect to photo page')
+        # write image like binary file in current working directory
+        with open('image.jpg', mode='wb') as file:
+            file.write(response.content)
+        logging.warning('successfully downloading image')
+    except HTTPError as e:
+        logging.error('HTTP error %s', (e))
+
 def main():
     logging.warning('search required photos')
-    search = py_un.search(type_='photos', query='redhead beauty', per_page=5)
+    search = py_un.search(type_='photos', query='redhead beaty', per_page=2)
     for photo in search.entries:
-        print(photo.link_download)
+        download_images(photo.body['urls']['full'])
 
 
 if __name__ == '__main__':
